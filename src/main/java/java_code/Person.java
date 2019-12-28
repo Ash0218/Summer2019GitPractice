f98ede2764ecf1b2c848571f2f33897bc20d0f62
Ash0218 <ajykim218@gmail.com>
2019-12-27 21:38:51 -0500

    Added constructor to Person class

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -4,4 +4,8 @@ public class Person {
     private int age; // 1
     private String firstName; // 2
     private String address; // 3
+
+    public Person (int age){
+        this.age = age;
+    }
 }
